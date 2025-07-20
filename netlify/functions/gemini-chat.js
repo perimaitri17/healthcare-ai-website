7466546c2b48ac1e628c7bdc19edfabd8093c062
perimaitri17 <maitriperi@gmail.com>
2025-07-21 01:22:52 +0530

    Update gemini-chat.js

--- netlify/functions/gemini-chat.js
+++ netlify/functions/gemini-chat.js
@@ -4,7 +4,7 @@ exports.handler = async (event, context) => {
   
   try {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`,
       {
         method: 'POST',
         headers: { 
